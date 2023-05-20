9ca01a0934c8fe429f34b3f8c813e3f2fff251cd
Laurent Dislaire <siladire@free.fr>
2023-05-20 21:41:59 +0200

    Routes Gget, Get:id et Post

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const parkings = require('./parkings.json')
+// Middleware
+app.use(express.json())
+// Route
+app.get('/parkings', (req, res) => { res.status(200).json(parkings) })
+app.get('/parkings/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const parking = parkings.find(parking => parking.id === id)
+  res.status(200).json(parking)
+})
+
+app.post('/parkings', (req, res) => {
+  parkings.push(req.body)
+  res.status(200).json(parkings)
+})
+
+app.listen(8080, () => { console.log("Serveur à l'écoute") })
