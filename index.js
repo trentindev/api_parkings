41edc9a68d2c8dedfe6d26f49ffa9c81aff0fba0
Laurent Dislaire <siladire@free.fr>
2023-05-22 11:12:20 +0200

    création des routes Post et Put

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express()
 const parkings = require('./parkings.json')
 // Middleware
 app.use(express.json())
-// Route
+
 app.get('/parkings', (req, res) => { res.status(200).json(parkings) })
+
 app.get('/parkings/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const parking = parkings.find(parking => parking.id === id)
@@ -12,8 +13,19 @@ app.get('/parkings/:id', (req, res) => {
 })
 
 app.post('/parkings', (req, res) => {
+  console.log(req.body)// Uniquement à des fin de dev
+
   parkings.push(req.body)
   res.status(200).json(parkings)
+})
+
+app.put('/parkings/:id', (req, res) => {
+  const id = parseInt(req.params.id)    
+  let parking = parkings.find(parking => parking.id === id)    
+  parking.name = req.body.name, 
+  parking.city = req.body.city, 
+  parking.type = req.body.type, 
+  res.status(200).json(parking)
 })
 
 app.listen(8080, () => { console.log("Serveur à l'écoute") })
