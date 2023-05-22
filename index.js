0b4c7df0da50ddae281696d0f3a8afb50ab3d5ad
Laurent Dislaire <siladire@free.fr>
2023-05-22 11:45:21 +0200

    Effacement des console.log de dev

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const parkings = require('./parkings.json')
 
 // Middleware
 app.use(express.json())
+
 // Liste de tous les parkings
 app.get('/parkings', (req, res) => { res.status(200).json(parkings) })
+
 // Données d'un parking particulier 
 app.get('/parkings/:id', (req, res) => {
   const id = parseInt(req.params.id)
@@ -13,9 +15,7 @@ app.get('/parkings/:id', (req, res) => {
   res.status(200).json(parking)
 })
 // Création d'un parking
-app.post('/parkings', (req, res) => {
-  console.log(req.body)// Uniquement à des fin de dev
-
+app.post('/parkings', (req, res) => {  
   parkings.push(req.body)
   res.status(200).json(parkings)
 })
@@ -31,12 +31,10 @@ app.put('/parkings/:id', (req, res) => {
 // Suppression d'un parking particulier
 app.delete('/parkings/:id', (req, res) => {
   const id = parseInt(req.params.id)    
-  let parking = parkings.find(parking => parking.id === id)
-  console.log(parkings.indexOf(parking))    
+  let parking = parkings.find(parking => parking.id === id)    
   parkings.splice(parkings.indexOf(parking), 1)    
   res.status(200).json(parkings)
 })
 
-
-
+// Mise en écoute du serveur
 app.listen(8080, () => { console.log("Serveur à l'écoute") })
